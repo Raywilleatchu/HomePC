0b47616fd05f3b47d514c83abc24d8523a8587a2
email <raymondmansour@quickenloans.com>
2021-10-15 19:08:17 -0400

    added functionality

--- ngPlay/ngPlay/src/app/calculator/calculator.component.ts
+++ ngPlay/ngPlay/src/app/calculator/calculator.component.ts
@@ -3,6 +3,7 @@ import { CalcButton } from '../calc-button';
 import { DOCUMENT } from '@angular/common';
 import { CalcBox } from '../calc-box';
 import { isNull } from '@angular/compiler/src/output/output_ast';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-calculator',
@@ -31,12 +32,54 @@ export class CalculatorComponent implements OnInit {
     return tb.boxText;
   }
 
-  doMath(tb:CalcBox):string{
+ DeleteEnd(tb:CalcBox):string
+ {
+  let endPoint = tb.boxText.length;
+  tb.boxText = tb.boxText.substr(0, endPoint - 1)
+  return tb.boxText;
+ }
+
+ ClearAll(tb:CalcBox):string
+ {
+  tb.boxText = ''
+  return tb.boxText;
+ }
+
+  doMath(tb:CalcBox):string
+  {
+  /*
+    Steps to do the math:
+    1) setup operands(number) to be saved as values 
+    2) run a loop within the string of numbers 
+    3) do the math accordingly in sets of 2
+  */
+
+    let operand1 = 0;
+    let operand2:Number;
+
     for(let i = 0;i<tb.boxText.length;i++)
     {
-      
+      if(tb.boxText[i] == '+')
+      {
+        if(operand1 == 0)
+        {
+          operand1 = parseInt(tb.boxText[i - 1]);
+        }
+        operand2 = parseInt(tb.boxText[i + 1]);
+        operand1 = (+operand1 + +operand2);
+      }
+      else if(tb.boxText[i] == '-')
+      {
+        if(operand1 == 0)
+        {
+          operand1 = parseInt(tb.boxText[i - 1]);
+        }
+        operand2 = parseInt(tb.boxText[i + 1]);
+        operand1 = (+operand1 - +operand2);
+      }
     }
-    return '';
+    tb.boxText = operand1.toString();
+    return tb.boxText;
   }
 
   constructor() { }
