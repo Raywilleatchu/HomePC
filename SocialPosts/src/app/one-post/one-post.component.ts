483aa272267ebe157d6f253c9a89e7bb1e20005e
Raywilleatchu <mansour_raymond@yahoo.com>
2021-10-25 16:58:58 -0400

    Lab Done For Now

---
+++ SocialPosts/src/app/one-post/one-post.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-one-post',
+  templateUrl: './one-post.component.html',
+  styleUrls: ['./one-post.component.css']
+})
+export class OnePostComponent implements OnInit {
+
+@Input() thepost:Post=
+{
+  title: "",
+  thought: ""
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
